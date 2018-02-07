409dbc8678c055a148baa73fce57902114240742
Michael <michael@garstka.org>
2018-02-07 15:12:52 +0000

    Working SOCP / SDP Example

--- examples/sdp_socp.jl
+++ examples/sdp_socp.jl
@@ -1,9 +1,10 @@
 # Test script to test solver for combined sdp and socp problem
 workspace()
 include("../Solver.jl")
+include("../Helper.jl")
 
-using Base.Test, Convex
-using OSSDP, OSSDPTypes, Mosek
+using Base.Test, Convex, Helper
+using OSSDP, OSSDPTypes, Mosek, SCS
 
 # Consider the following problem that includes a second order cone and a sdp constraint
 # min c'x
@@ -13,16 +14,15 @@ using OSSDP, OSSDPTypes, Mosek
 
 # Generate problem data
 
-A = [0. 0 0 0 1 1 1 1 1 1 1 1 1; #sum(x(5:13) = 18)
-    1 0 0 -3 0 0 0 0 0 0 0 0 0; #( t = 3*x[4] )
-     0  1 -1 0 0 0 0 0 0 0 0 0 0; #(x[2] = x[3])
-     0 0 0 0 0 1 0 0 0 0 0 0 0;
-     0 0 0 0 0 0 1 0 0 0 0 0 0;
-     0 0 0 0 0 0 0 0 0 0 0 0 1;
+A = [0 1 0 0 0 0 0 0 0 0 0 0 0; #( x[2] = 2 )
+     0 2 -1 0 0 0 0 0 0 0 0 0 0; #(2x[2] = x[3])
+     0 1 0 0 -1 0 0 0 0 0 0 0 0; #x[2] = X[1,1]
+     0 0 1 0 0 0 0 0 -1 0 0 0 0; #x[3] = X[2,2]
+     0 0 0 1 0 0 0 0 0 0 0 0 -1; #x[4] = X[3,3]
      ]
-b = [18.; 0; 0; 3; 2; 3]
+b = [2;0;0;0;0]
 
-c = [5.;1;1;1;3*ones(9,1)]
+c = [5.;1;1;0;zeros(9,1)]
 
 
 #####
@@ -40,41 +40,46 @@ settings = sdpSettings(rho=1.0,sigma=1.0,alpha=1.6,max_iter=2500,verbose=true,ch
 res = solveSDP(P,c,A,b,K,settings)
 t = res.x[1]
 x1 = res.x[2:4]
-x2 = res.x[5:13]
+x2 = reshape(res.x[5:13],3,3)
 
 
 # #####
 # # Solve problem with Convex + MOSEK
 # #####
 
-# t2 = Variable(n)
-# z2 = Variable(n)
-# w2 = Variable(1)
-# problem = minimize( 0.5*w2 + μ*sum(t2))
-# problem.constraints += z2 <= t2
-# problem.constraints += z2 >= -t2
-# problem.constraints += norm([1-w2;2*(F*z2-b)],2) <= 1 + w2
+x = Variable(4)
+X = Variable(3,3)
 
-# solve!(problem,MosekSolver())
+problem = minimize( c[1:4]'*x)
+problem.constraints += x[2] == 2
+problem.constraints += 2*x[2] == x[3]
+problem.constraints += X[1,1] == x[2]
+problem.constraints += X[2,2] == x[3]
+problem.constraints += X[3,3] == x[4]
 
-# @testset "Run all tests" begin
-  @testset "Lasso (SOCP) Problem OSSDP " begin
-    @test norm(x1,2) - t<= 1e-5
-    @test isposdef(reshape(x2,3,3))
+problem.constraints += norm(x[2:4],2) <= x[1]
+problem.constraints +=  isposdef(X)
+
+
+solve!(problem,MosekSolver())
+
+ @testset "Run all tests" begin
+  @testset "Combined SOCP SDP Problem - OSSDP" begin
+    @test abs(norm(x1,2) - t)<= 1e-5
+    @test isNumericallyPosSemDef(reshape(x2,3,3),1e-6)
     @test norm(A*res.x - b,Inf) <= 1e-4
   end
 
-#   @testset "Lasso (SOCP) Problem Mosek " begin
-#     @test norm([1-w2.value;2*(F*z2.value-b)],Inf) <= 1 + w2.value
-#     @test minimum(z2.value + t2.value) >= -1e-5
-#     @test maximum(z2.value - t2.value) <= 1e-5
-#   end
+  @testset "Combined SOCP SDP Problem - Mosek " begin
+    @test abs(norm(x.value[2:4],2) - x.value[1]) <= 1e-5
+    @test isNumericallyPosSemDef(X.value,1e-6)
+  end
 
-#   @testset "Compare solvers " begin
-#     @test norm(z-z2.value,Inf) <= 1e-3
-#     @test abs(res.cost - problem.optval) <= 1e-3
-#   end
+  @testset "Compare solvers " begin
+    @test norm(res.x[1:4]-x.value,Inf) <= 1e-3
+    @test abs(res.cost - problem.optval) <= 1e-3
+  end
 
-# end
+end
 # nothing
 
