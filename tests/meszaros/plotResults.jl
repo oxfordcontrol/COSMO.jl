b17e5174dd661c9abeb4fa6192ddaf23efc2307b
nrontsis <nrontsis@gmail.com>
2018-05-21 19:04:32 +0100

    #30 changes in plotting

--- tests/meszaros/plotResults.jl
+++ tests/meszaros/plotResults.jl
@@ -2,14 +2,14 @@ using PyPlot, JLD
 
 fn = "qocs_vs_osqp_meszaros.jld"
 data = JLD.load(fn)
-n = length(data["filenames"])
+n = length(data["fileNames"])
 
 println("""QOCS success ratio: $(data["success"]/n)""")
-println("""OSQP success ratio: $(data["successOSQP"]n)""")
+println("""OSQP success ratio: $(data["successOSQP"]/n)""")
 
 using PyPlot
 figure(1)
-semilogy(2:n, [data["iters"] data["itersOSQP"]], linestyle="None", marker=".")
+semilogy(2:n, [data["iters"][2:end] data["itersOSQP"][2:end]], linestyle="None", marker=".")
 title("Number of Iterations in the Maros-Mescaros Dataset")
 legend(["QOCS", "OSQP"])
 xlabel("Problem index (sorted by nnz)")
@@ -17,7 +17,7 @@ ylabel("Iterations")
 savefig("iter.pdf")
 
 figure(2)
-semilogy(2:n, 1000*[data["times"] data["timesOSQP"]], linestyle="None", marker=".")
+semilogy(2:n, 1000*[data["times"][2:end] data["timesOSQP"][2:end]], linestyle="None", marker=".")
 title("Average per iteration time for the Maros-Mescaros Dataset")
 legend(["QOCS", "OSQP"])
 xlabel("Problem index (sorted by nnz)")
