b00afcacc66a1da855a9aec20fe86abe3e929835
Michael <michael@garstka.org>
2018-08-03 13:06:00 +0100

    Remove JuMP package

--- tests/UnitTests/runTests.jl
+++ tests/UnitTests/runTests.jl
@@ -1,13 +1,13 @@
 include("../../src/Solver.jl")
 
-using JuMP, OSSDP, OSSDPTypes, Base.Test
+using OSSDP, OSSDPTypes, Base.Test
 
 rng = MersenneTwister(12345)
 
 @testset "All tests" begin
 
   include("./simple.jl")
-   include("./qp-lasso.jl")
+  include("./qp-lasso.jl")
   include("./socp-lasso.jl")
   include("./closestcorr.jl")
   include("./InfeasibilityTests/runTests.jl")
