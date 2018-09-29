2fc159755b597f4144190ad539c8d5d742e69723
Paul Goulart <paul.goulart@eng.ox.ac.uk>
2018-09-29 23:37:52 +0100

    r/lmul identity case

--- src/Algebra.jl
+++ src/Algebra.jl
@@ -1,78 +1,89 @@
 import LinearAlgebra
 
 function colNorms!(v::Array{Tf,1},
-                   A::Matrix{Tf};
-                   reset::Bool = true) where{Tf<:AbstractFloat}
-  startFromZero && v.= 0
-  for i=1:size(A,2)
-    v[i] = max(v[i],norm(view(A,:,i),Inf))
-  end
+    A::Matrix{Tf};
+    reset::Bool = true) where{Tf<:AbstractFloat}
+    startFromZero && v.= 0
+    for i=1:size(A,2)
+        v[i] = max(v[i],norm(view(A,:,i),Inf))
+    end
 end
 
 function colNorms!(v::Array{Tf,1},
-                   A::SparseMatrixCSC{Tf,Ti};
-                   reset::Bool = true) where{Tf<:AbstractFloat,Ti<:Integer}
-  startFromZero && v.= 0
-  for i=1:A.n
-    for j = A.colptr[i]:(A.colptr[i+1] - 1)
-        v[i] = max(v[i],abs(A.nzval[j]))
+    A::SparseMatrixCSC{Tf,Ti};
+    reset::Bool = true) where{Tf<:AbstractFloat,Ti<:Integer}
+    startFromZero && v.= 0
+    for i=1:A.n
+        for j = A.colptr[i]:(A.colptr[i+1] - 1)
+            v[i] = max(v[i],abs(A.nzval[j]))
+        end
     end
-  end
 end
 
 function rowNorms!(v::Array{Tf,1},
-                   A::Matrix{Tf};
-                   reset::Bool = true) where{Tf<:AbstractFloat}
-  startFromZero && v.= 0
-  for i=1:size(A,1)
-    v[i] = max(v[i],norm(view(A,i,:),Inf))
-  end
+    A::Matrix{Tf};
+    reset::Bool = true) where{Tf<:AbstractFloat}
+    startFromZero && v.= 0
+    for i=1:size(A,1)
+        v[i] = max(v[i],norm(view(A,i,:),Inf))
+    end
 end
 
 
 
 function rowNorms!(v::Array{Tf,1},
-                   A::SparseMatrixCSC{Tf,Ti};
-                   reset::Bool = true) where{Tf<:AbstractFloat,Ti<:Integer}
+    A::SparseMatrixCSC{Tf,Ti};
+    reset::Bool = true) where{Tf<:AbstractFloat,Ti<:Integer}
 
-  startFromZero && v.= 0
-  for i=1:(A.colptr[end]-1)
-     v[A.rowval[i]] = max(v[A.rowval[i]],abs(A.nzval[i]))
-  end
+    startFromZero && v.= 0
+    for i=1:(A.colptr[end]-1)
+        v[A.rowval[i]] = max(v[A.rowval[i]],abs(A.nzval[i]))
+    end
 
 end
 
 
+function lmul!(L::Identity{Bool}, M)
+    return M
+end
 
-  function lmul!(L::Diagonal, M::SparseMatrixCSC)
+function rmul!(M,R::Identity{Bool})
+    return M
+end
 
-      #NB : Same as:  @views M.nzval .*= D.diag[M.rowval]
-      #but this way allocates no memory at all and
-      #is marginally faster
-      for i = 1:(M.colptr[end]-1)
-          M.nzval[i] *= L.diag[M.rowval[i]]
-      end
-      M
-  end
+function lmul!(L::Diagonal, M::SparseMatrixCSC)
 
-  function rmul!(M::SparseMatrixCSC,R::Diagonal)
-      for i = 1:M.n
-          for j = M.colptr[i]:(M.colptr[i+1]-1)
-              M.nzval[j] *= R.diag[i]
-          end
-      end
-      M
-  end
+    #NB : Same as:  @views M.nzval .*= D.diag[M.rowval]
+    #but this way allocates no memory at all and
+    #is marginally faster
+    for i = 1:(M.colptr[end]-1)
+        M.nzval[i] *= L.diag[M.rowval[i]]
+    end
+    M
+end
 
-  function lrmul!(L::Diagonal, M::AbstractMatrix, R::Diagonal)
-      lmul!(L,rmul!(M,R))
-  end
+function rmul!(M::SparseMatrixCSC,R::Diagonal)
+    for i = 1:M.n
+        for j = M.colptr[i]:(M.colptr[i+1]-1)
+            M.nzval[j] *= R.diag[i]
+        end
+    end
+    M
+end
 
-  function lrmul!(L::Diagonal, M::SparseMatrixCSC, R::Diagonal)
-      for i = 1:M.n
-          for j = M.colptr[i]:(M.colptr[i+1]-1)
-              M.nzval[j] *= L.diag[M.rowval[j]]*R.diag[i]
-          end
-      end
-      M
-  end
+function lrmul!(L::Identity{Bool}, M::AbstractMatrix, R::Identity{Bool})
+    return M
+end
+
+function lrmul!(L::Diagonal, M::AbstractMatrix, R::Diagonal)
+    lmul!(L,rmul!(M,R))
+end
+
+function lrmul!(L::Diagonal, M::SparseMatrixCSC, R::Diagonal)
+    for i = 1:M.n
+        for j = M.colptr[i]:(M.colptr[i+1]-1)
+            M.nzval[j] *= L.diag[M.rowval[j]]*R.diag[i]
+        end
+    end
+    M
+end
