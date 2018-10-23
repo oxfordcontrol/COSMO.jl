23e519b37b70919e6c456c1460d8a0aeb08d7725
nrontsis <nrontsis@gmail.com>
2018-10-23 18:01:14 +0100

    #42 Testing on the optimal eigenvalues sdp example from Pataki 1998.

---
+++ optimal_eigenvalues.jl
@@ -0,0 +1,55 @@
+using COSMO, Test, LinearAlgebra, Random, SparseArrays
+using JuMP, SCS, LinearAlgebra
+
+n = 700;
+m = 50;
+k = 10;
+rng = Random.MersenneTwister(12345)
+A = []
+for i = 1:m+1
+    A_i = sprandn(rng, n, n, 0.001/2); A_i = (A_i + A_i')/2
+    push!(A, A_i)
+end
+#@testset "Lanczos Projection - Simple SDP" begin
+eye = Matrix{Float64}(I, n, n)
+c = [eye[:]; zeros(n^2); k; zeros(m)]; c = reshape(c, length(c), 1)
+P = spzeros(length(c), length(c))
+speye = spdiagm(0 => ones(n^2))
+speye_small = spdiagm(0 => ones(n))
+global A_1 = [speye -speye speye_small[:]]
+for i = 1:m
+    global A_1 = [A_1 -A[i+1][:]]
+end
+b_1 = -Array(A[1][:]); b_1 = reshape(b_1, length(b_1), 1)
+zero_constraint = COSMO.Constraint(A_1, b_1, COSMO.ZeroSet)
+A_ = [speye spzeros(n^2, n^2 + m + 1)]
+b_ = zeros(size(A_, 1))
+conic_constraint_1 = COSMO.Constraint(A_, b_, COSMO.PsdCone)
+A_ = [spzeros(n^2, n^2) speye spzeros(n^2, m + 1)]
+b_ = zeros(size(A_, 1), 1)
+conic_constraint_2 = COSMO.Constraint(A_, b_, COSMO.PsdCone)
+
+# define example problem
+settings = COSMO.Settings(verbose=true, max_iter=2500)
+
+model = COSMO.Model()
+assemble!(model,P,c,[zero_constraint; conic_constraint_1; conic_constraint_2])
+
+res = COSMO.optimize!(model,settings);
+
+#=
+model = JuMP.Model()
+setsolver(model, SCSSolver(max_iters=10000, acceleration_lookback=1))
+@variable(model, V[1:n,1:n], SDP)
+@variable(model, W[1:n,1:n], SDP)
+@variable(model, z)
+@variable(model, x[1:m])
+@objective(model, Min, k*z + sum(diag(V)))
+@constraint(model, z.*eye + V - W .== A[1] + x[1].*A[2] + x[2].*A[3] + x[3].*A[4] + x[4].*A[5] + x[5].*A[6])
+status = JuMP.solve(model)
+@show getvalue(z)
+=#
+
+@show res.x[2*n^2+1]
+
+nothing
