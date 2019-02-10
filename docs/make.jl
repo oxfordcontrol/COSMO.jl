6175441bd35ef292115bebb2d68748712135a169
Michael <michael@garstka.org>
2019-02-10 16:46:44 +0000

    Add tracking ID

--- docs/make.jl
+++ docs/make.jl
@@ -7,6 +7,7 @@ makedocs(
         canonical = "https://oxfordcontrol.github.io/COSMO.jl/stable/",),
   assets = ["assets/favicon.ico","assets/github_buttons.js"],
   authors = "Michael Garstka and contributors.",
+  analytics = "UA-134239283-1",
   pages = [
         "Home" => "index.md",
         "User Guide" => "guide.md",
