8553294fcc403c521df8b9200fd96c4cbad5f41e
nrontsis <nrontsis@gmail.com>
2019-09-02 17:55:25 +0100

    Indirect solvers tested with documentation.

---
+++ src/kktsolver_indirect.jl
@@ -0,0 +1,170 @@
+using LinearMaps, IterativeSolvers
+
+mutable struct IndirectReducedKKTSolver{TP, TA, T} <: COSMO.AbstractKKTSolver
+    m::Integer
+    n::Integer
+    P::TP
+    A::TA
+    σ::T
+    ρ::Vector{T}
+    tol_constant::T
+    tol_exponent::T
+    # Memory and statistics for cg
+    solver_type::Symbol
+    previous_solution::Vector{T}
+    iteration_counter::Int
+    multiplications::Vector{Int}
+    tmp_n::Vector{T} # n-dimensional used to avoid memory allocation
+    tmp_m::Vector{T} # m-dimensional used to avoid memory allocation
+
+    function IndirectReducedKKTSolver(P::TP, A::TA, σ::T, ρ;
+        solver_type::Symbol=:CG, tol_constant::T=T(1.0), tol_exponent::T=T(1.5)
+        ) where {TP, TA, T}
+        m, n = size(A)
+        if isa(ρ, T)
+            ρ = ρ*ones(T, m)
+        end
+        @assert eltype(P) == eltype(A) == T "Inconsistent element types."
+        @assert solver_type == :CG || solver_type == :MINRES "Solver symbol must be either :CG or :MINRES"
+        new{TP, TA, T}(m, n, P, A, σ, ρ,
+            tol_constant, tol_exponent,
+            solver_type,
+            zeros(T, n), 1, zeros(Int, 0), zeros(T, n), zeros(T, m))
+    end
+end
+
+function solve!(S::IndirectReducedKKTSolver, y::AbstractVector{T}, x::AbstractVector{T}) where {T}
+    # Solves the (KKT) linear system
+    # | P + σI     A'  |  |y1|  =  |x1|
+    # | A        -I/ρ  |  |y2|  =  |x2|
+    # x1,y1: R^n, x2/y2: R^m
+    # where [y1; y2] := y, [x1; x2] := x 
+
+    # In particular we perform cg/minres on the reduced system
+    # (P + σΙ + Α'ρΑ)y1 = x1 + A'ρx2
+    # And then recover y2 as
+    # y2 = ρ(Ay1 - x2)
+
+    x1 = view(x, 1:S.n); y1 = view(y, 1:S.n)
+    x2 = view(x, S.n+1:S.n+S.m); y2 = view(y, S.n+1:S.n+S.m)
+
+    # Form right-hand side for cg/minres: y1 = x1 + A'ρx2
+    @. y2 = S.ρ*x2
+    mul!(y1, S.A', y2)
+    y1 .+= x1
+
+    push!(S.multiplications, 0)
+    function reduced_mul!(y::AbstractVector{T}, x::AbstractVector{T}) where {T}
+        # y = (P + σΙ + Α'ρΑ)x
+        mul!(S.tmp_m, S.A, x)
+        @. S.tmp_m .*= S.ρ
+        mul!(S.tmp_n, S.A', S.tmp_m)
+        axpy!(S.σ, x, S.tmp_n)
+        mul!(y, S.P, x)
+        axpy!(one(T), S.tmp_n, y)
+        S.multiplications[end] += 1
+        return y
+    end
+    L = LinearMap(reduced_mul!, S.n; ismutating=true, issymmetric=true)
+    if S.solver_type == :CG
+        cg!(S.previous_solution, L, y1, tol=get_tolerance(S)/norm(y1))
+    elseif S.solver_type == :MINRES
+        init_residual = norm(L*S.previous_solution - y1)
+        minres!(S.previous_solution, L, y1, tol=get_tolerance(S)/init_residual)
+    end
+    # Sanity check for tolerance
+    # might not always hold for MINRES, as its termination criterion is approximate, (see its documentation)
+    # @assert get_tolerance(S) > norm(L*S.previous_solution - y1)
+    copyto!(y1, S.previous_solution)
+
+    # y2 = Ay1 - x2
+    mul!(y2, S.A, y1)
+    axpy!(-one(T), x2, y2)
+    @. y2 .*= S.ρ
+
+    S.iteration_counter += 1
+
+    return y
+end
+
+mutable struct IndirectKKTSolver{TP, TA, T} <: COSMO.AbstractKKTSolver
+    m::Integer
+    n::Integer
+    P::TP
+    A::TA
+    σ::T
+    ρ::Vector{T}
+    tol_constant::T
+    tol_exponent::T
+    # Memory and statistics for cg
+    solver_type::Symbol
+    previous_solution::Vector{T}
+    iteration_counter::Int
+    multiplications::Vector{Int}
+    tmp_n::Vector{T} # n-dimensional used to avoid memory allocation
+    tmp_m::Vector{T} # m-dimensional used to avoid memory allocation
+
+    function IndirectKKTSolver(P::TP, A::TA, σ::T, ρ; solver_type::Symbol=:MINRES,
+        tol_constant::T=T(1.0), tol_exponent::T=T(1.5)
+        ) where {TP, TA, T}
+        m, n = size(A)
+        if isa(ρ, T)
+            ρ = ρ*ones(T, m)
+        end
+        @assert eltype(P) == eltype(A) == T "Inconsistent element types."
+        @assert solver_type == :MINRES "Solver symbol must be :MINRES"
+        new{TP, TA, T}(m, n, P, A, σ, ρ,
+            tol_constant, tol_exponent,
+            solver_type,
+            zeros(T, m + n), 1, zeros(Int, 0), zeros(T, n), zeros(T, m))
+    end
+end
+
+function solve!(S::IndirectKKTSolver, y::AbstractVector{T}, x::AbstractVector{T}) where {T}
+    # Solves the (KKT) linear system
+    # | P + σI     A'  |  |y1|  =  |x1|
+    # | A        -I/ρ  |  |y2|  =  |x2|
+    # x1,y1: R^n, x2/y2: R^m
+    # where [y1; y2] := y, [x1; x2] := x 
+    push!(S.multiplications, 0)
+    function kkt_mul!(y::AbstractVector{T}, x::AbstractVector{T}) where {T}
+        # Performs
+        # |y1| = | P + σI     A'  |  |x1|
+        # |y2| = | A        -I/ρ  |  |x2|
+        x1 = view(x, 1:S.n); y1 = view(y, 1:S.n)
+        x2 = view(x, S.n+1:S.n+S.m); y2 = view(y, S.n+1:S.n+S.m)
+
+        mul!(S.tmp_n, S.A', x2)
+        axpy!(S.σ, x1, S.tmp_n)
+        mul!(y1, S.P, x1)
+        axpy!(one(T), S.tmp_n, y1)
+
+        @. y2 = -x2/S.ρ
+        mul!(S.tmp_m, S.A, x1)
+        axpy!(one(T), S.tmp_m, y2)
+
+        S.multiplications[end] += 1
+        return y
+    end
+    L = LinearMap(kkt_mul!, S.n + S.m; ismutating=true, issymmetric=true)
+    if S.solver_type == :MINRES
+        init_residual = norm(L*S.previous_solution - y)
+        minres!(S.previous_solution, L, y, tol=get_tolerance(S)/init_residual)
+    end
+    # Sanity check for tolerance
+    # might not always hold for MINRES, as its termination criterion is approximate, (see its documentation)
+    # @assert get_tolerance(S) > norm(L*S.previous_solution - y)
+    copyto!(y, S.previous_solution)
+
+    S.iteration_counter += 1
+
+    return y
+end
+
+function update_rho!(S::Union{IndirectReducedKKTSolver, IndirectKKTSolver}, ρ)
+    S.ρ .= ρ
+end
+
+function get_tolerance(S::Union{IndirectReducedKKTSolver, IndirectKKTSolver})
+    return S.tol_constant/S.iteration_counter^S.tol_exponent
+end
--- test/UnitTests/kktsolver.jl
+++ test/UnitTests/kktsolver.jl
@@ -1,5 +1,9 @@
 using COSMO, Test, LinearAlgebra, SparseArrays, Random, QDLDL, Pkg
-rng = Random.MersenneTwister(2401)
+include("COSMOTestUtils.jl")
+
+rng = Random.MersenneTwister(1)
+
+T = Float64
 
 function make_test_kkt(P, A, sigma, rho)
 
@@ -11,39 +15,99 @@ function make_test_kkt(P, A, sigma, rho)
     return K
 end
 
-solver_types = [COSMO.QdldlKKTSolver
-                COSMO.CholmodKKTSolver]
+function add_kwargs(array; kwargs...)
+    push!(array, kwargs)
+end
+
+solver_types = []
+solver_tols = []
+params = []
+push!(solver_types, COSMO.QdldlKKTSolver)
+push!(solver_tols, 1e-10)
+add_kwargs(params)
+push!(solver_types, COSMO.CholmodKKTSolver)
+push!(solver_tols, 1e-10)
+add_kwargs(params)
+# solver_types = [COSMO.QdldlKKTSolver
+                # COSMO.CholmodKKTSolver]
 
 # optional dependencies
 if in("Pardiso",keys(Pkg.installed()))
     using Pardiso
-    Pardiso.MKL_PARDISO_LOADED[] && push!(solver_types, COSMO.MKLPardisoKKTSolver)
-    Pardiso.PARDISO_LOADED[]     && push!(solver_types, COSMO.PardisoDirectKKTSolver)
-    Pardiso.PARDISO_LOADED[]     && push!(solver_types, COSMO.PardisoIndirectKKTSolver)
+    if Pardiso.MKL_PARDISO_LOADED[] 
+        push!(solver_types, COSMO.MKLPardisoKKTSolver)
+        push!(solver_tols, 1e-10)
+            add_kwargs(params)
+        end
+    if Pardiso.PARDISO_LOADED[] 
+        push!(solver_types, COSMO.PardisoDirectKKTSolver)
+        push!(solver_tols, 1e-10)
+        add_kwargs(params)
+    end
+    if Pardiso.PARDISO_LOADED[]
+        push!(solver_types, COSMO.PardisoIndirectKKTSolver)
+        push!(solver_tols, 5e-5)
+        add_kwargs(params)
+    end
 end
 
-solver_tols   = [1e-10, 1e-10, 1e-10, 1e-10, 5e-5]
+if in("IterativeSolvers",keys(Pkg.installed()))
+    using IterativeSolvers
+    push!(solver_types, COSMO.IndirectReducedKKTSolver)
+    push!(solver_tols, 1e-3)
+    add_kwargs(params, solver_type=:CG)
 
- @testset "$(solver_types[i]) : KKT solver tests" for i = 1:length(solver_types)
+    push!(solver_types, COSMO.IndirectReducedKKTSolver)
+    push!(solver_tols, 1e-3)
+    add_kwargs(params, solver_type=:MINRES)
+
+    push!(solver_types, COSMO.IndirectKKTSolver)
+    push!(solver_tols, 1e-3)
+    add_kwargs(params, solver_type=:MINRES)
+end
+
+
+ @testset "$(solver_types[i]) $(params[i]) : KKT solver tests" for i = 1:length(solver_types)
 
     m = 10
     n = 20
 
-    for rho1 in [rand(1), rand(1)[], rand(m)],
-        rho2 in [rand(1), rand(m)],
-        sigma in [rand(1), rand(1)[], rand(n)]
+    for rho1 in [rand(T), rand(T, m)],
+        rho2 in [rand(T), rand(T, m)],
+        sigma in [rand(T)]
 
         P  = sparse(generate_pos_def_matrix(rng, n))
         A  = sprandn(m, n, 0.2)
         b = randn(m + n)
 
-        F = solver_types[i](P, A, sigma, rho1)
+        F = solver_types[i](P, A, sigma, rho1; params[i]...)
+        if isa(F, COSMO.IndirectReducedKKTSolver) || isa(F, COSMO.IndirectKKTSolver)
+            # Technically, we should have been able to set the tolerance even lower
+            # But, currently, issues in IterativeSolvers.jl do not allow this
+            # See: https://github.com/JuliaMath/IterativeSolvers.jl/pull/244
+            F.iteration_counter = 10^4 # This forces CG's/MINRES tolerance to be 1/10^4
+        end
         J = make_test_kkt(P, A, sigma, rho1)
         x = copy(b)
 
         #test a single solve
         COSMO.solve!(F, x, b)
         @test norm(x - J \ b) <= solver_tols[i]
+
+        # Check that warm starting works
+        # Invoking again an indirect solver should result in the solution with only
+        # one matrix vector multiplication
+        if isa(F, COSMO.IndirectReducedKKTSolver) || isa(F, COSMO.IndirectKKTSolver)
+            # The calculation of the residual, and thus the termination criterion, of
+            # MINRES is approximate. Thus warm started solutions won't necessarily finish in one step
+            # For this reason we don't check warm starting with MINRES for now :(
+            if F.solver_type != :MINRES
+                COSMO.solve!(F, x, b)
+                @show F.multiplications
+                @test F.multiplications[end] <= 1 
+                @test norm(x - J \ b) <= solver_tols[i]
+            end
+        end
 
         #test a rho update and solve
         J = make_test_kkt(P, A, sigma, rho2)
