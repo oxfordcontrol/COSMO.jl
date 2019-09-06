65b1e164e4101464752c415c373e962c656d1194
Michael <michael@garstka.org>
2019-09-06 14:21:29 +0100

    Split unit tests into two files
    - Split unit tests into COSMO-native tests and MOI wrapper tests
    - Make `runtests.jl` call both scripts
    - This allows us to tests code changes unrelated to MOI more efficiently

---
+++ test/run_cosmo_tests.jl
@@ -0,0 +1,31 @@
+using COSMO, Random, Test, Pkg
+rng = Random.MersenneTwister(12345)
+
+include("./UnitTests/COSMOTestUtils.jl")
+
+@testset "COSMO native tests" begin
+
+  include("./UnitTests/simple.jl")
+  include("./UnitTests/sets.jl")
+  include("./UnitTests/constraints.jl")
+  include("./UnitTests/kktsolver.jl")
+  include("./UnitTests/model.jl")
+  include("./UnitTests/qp-box.jl")
+  include("./UnitTests/socp-lasso.jl")
+  include("./UnitTests/closestcorr.jl")
+  include("./UnitTests/exp_cone.jl")
+  include("./UnitTests/pow_cone.jl")
+  include("./UnitTests/print.jl")
+  include("./UnitTests/InfeasibilityTests/runTests.jl")
+  include("./UnitTests/algebra.jl")
+  include("./UnitTests/splitvector.jl")
+  include("./UnitTests/interface.jl")
+  include("./UnitTests/chordal_decomposition_triangle.jl")
+  include("./UnitTests/psd_completion.jl")
+
+    # optional unittests
+  if in("Pardiso",keys(Pkg.installed()))
+    include("./UnitTests/options_factory.jl")
+  end
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,33 +1,12 @@
-
-using COSMO, Random, Test, Pkg
-rng = Random.MersenneTwister(12345)
+using COSMO, Random, Test
 
 include("./UnitTests/COSMOTestUtils.jl")
 
 @testset "All Unit Tests" begin
 
-  include("./UnitTests/simple.jl")
-  include("./UnitTests/sets.jl")
-  include("./UnitTests/constraints.jl")
-  include("./UnitTests/kktsolver.jl")
-  include("./UnitTests/model.jl")
-  include("./UnitTests/qp-box.jl")
-  include("./UnitTests/socp-lasso.jl")
-  include("./UnitTests/closestcorr.jl")
-  include("./UnitTests/exp_cone.jl")
-  include("./UnitTests/pow_cone.jl")
-  include("./UnitTests/print.jl")
-  include("./UnitTests/InfeasibilityTests/runTests.jl")
-  include("./UnitTests/algebra.jl")
-  include("./UnitTests/splitvector.jl")
-  include("./UnitTests/interface.jl")
-  include("./UnitTests/chordal_decomposition_triangle.jl")
-  include("./UnitTests/psd_completion.jl")
+  # Split tests into native interface unit tests and MOI unit tests
+  include("./run_cosmo_tests.jl")
   include("./UnitTests/moi_wrapper.jl")
 
-  # optional unittests
-  if in("Pardiso",keys(Pkg.installed()))
-    include("./UnitTests/options_factory.jl")
-  end
 end
 nothing
