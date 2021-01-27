f8064bfd683d9209b3f2dee68f5a4cd7ffb2e7b9
Michael Garstka <michael@garstka.org>
2021-01-27 16:45:59 +0000

    Fix max rho unit test
    - Add acceleration test script to cosmo tests

--- test/UnitTests/Acceleration/max_rho_adaption.jl
+++ test/UnitTests/Acceleration/max_rho_adaption.jl
@@ -11,13 +11,12 @@ using COSMO, SparseArrays, LinearAlgebra, Test
     l = [1.; 0; 0];
     u = [1; 0.7; 0.7];
 
-    # First, we decide to solve the problem with two one-sided constraints using `COSMO.Nonnegatives` as the convex set:
     Aa = [-A; A]
     ba = [u; -l]
     constraint1 = COSMO.Constraint(Aa, ba, COSMO.Nonnegatives);
 
     # Next, we define the settings object, the model and then assemble everything:
-    settings = COSMO.Settings(verbose=true, adaptive_rho_interval = 25, adaptive_rho_max_adaptions = 2, rho = 1e-4, eps_abs = 1e-4, eps_rel = 1e-4);
+    settings = COSMO.Settings(verbose = false, adaptive_rho_interval = 25, adaptive_rho_max_adaptions = 2, rho = 1e-6, eps_abs = 1e-6, eps_rel = 1e-4);
     model = COSMO.Model();
     assemble!(model, P, q, constraint1, settings = settings);
     res = COSMO.optimize!(model);
@@ -25,7 +24,7 @@ using COSMO, SparseArrays, LinearAlgebra, Test
     @test num_rho_adaptions(model) == 2
 
     # Solve again but restrict number of rho updates to 1
-   settings = COSMO.Settings(verbose=true, adaptive_rho_interval = 25, adaptive_rho_max_adaptions = 1, rho = 1e-4, eps_abs = 1e-4, eps_rel = 1e-4);
+    settings = COSMO.Settings(verbose = false, adaptive_rho_interval = 25, adaptive_rho_max_adaptions = 1, rho = 1e-6, eps_abs = 1e-4, eps_rel = 1e-4);
     model = COSMO.Model();
     assemble!(model, P, q, constraint1, settings = settings);
     res = COSMO.optimize!(model);
