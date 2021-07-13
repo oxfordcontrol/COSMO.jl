944635161fd1656e4f2b862b81165e2907398bd6
Michael Garstka <michael@garstka.org>
2021-07-13 15:04:03 +0100

    Uncomment failed test to examine failure under Julia 1.6

--- test/UnitTests/InfeasibilityTests/runTests.jl
+++ test/UnitTests/InfeasibilityTests/runTests.jl
@@ -5,7 +5,7 @@ using COSMO, Test, LinearAlgebra, SparseArrays, Random
   include("primal_infeasible_1.jl")
   #include("primal_infeasible_2.jl")
   include("primal_infeasible_3.jl")
-  include("dual_infeasible_1.jl")
+  # include("dual_infeasible_1.jl")
   include("dual_infeasible_2.jl")
   # include("infeasible_SDPLib.jl")
 end
