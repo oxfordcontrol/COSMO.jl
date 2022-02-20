04cca33ae7b401200726a1ac45d74f430788c8d8
Michael Garstka <michael@garstka.org>
2022-02-20 17:13:26 +0000

    Reorganise src files into folders

--- src/COSMO.jl
+++ src/COSMO.jl
@@ -14,13 +14,13 @@ const DefaultFloat = Float64
 const DefaultInt   = LinearAlgebra.BlasInt
 
 
-include("./kktsolver.jl")
+include("./linear_solver/kktsolver.jl")
 # optional dependencies
 function __init__()
-    @require Pardiso="46dd5b70-b6fb-5a00-ae2d-e8fea33afaf2" include("kktsolver_pardiso.jl")
+    @require Pardiso="46dd5b70-b6fb-5a00-ae2d-e8fea33afaf2" include("./linear_solver/kktsolver_pardiso.jl")
 
     @require IterativeSolvers="42fd0dbc-a981-5370-80f2-aaf504508153" begin
-        @require LinearMaps="7a12625a-238d-50fd-b39a-03d52299707e" include("./kktsolver_indirect.jl")
+        @require LinearMaps="7a12625a-238d-50fd-b39a-03d52299707e" include("./linear_solver/kktsolver_indirect.jl")
     end
 end
 
@@ -31,10 +31,9 @@ end
 
 include("./algebra.jl")
 include("./projections.jl")
-#include("./acceleration.jl")
-include("./trees.jl")
-include("./clique_graph.jl")
-include("./clique_merging.jl")
+include("./chordal_decomposition/trees.jl")
+include("./chordal_decomposition/clique_graph.jl")
+include("./chordal_decomposition/clique_merging.jl")
 include("./settings.jl")
 include("./types.jl")
 include("./accelerator_interface.jl")
@@ -44,15 +43,13 @@ include("./parameters.jl")
 include("./residuals.jl")
 include("./scaling.jl")
 include("./infeasibility.jl")
-include("./transformations.jl")
-include("./chordal_decomposition.jl")
+include("./chordal_decomposition/transformations.jl")
+include("./chordal_decomposition/chordal_decomposition.jl")
 include("./printing.jl")
 include("./setup.jl")
 include("./solver.jl")
 include("./interface.jl")
 include("./MOI_wrapper.jl")
 
-# include("precompile.jl")
-# _precompile_()
 
 end #end module
